d3691ebbbd588ea7a497d429b9aaac941be13714
sheikhsultan <sheikhsultanm@gmail.com>
2020-07-17 00:36:40 +0600

    some function added with Q's

---
+++ assignment.js
@@ -0,0 +1,28 @@
+/** feet-to-mile convert */
+function feetToMile(params) {
+    
+}
+
+/** wood-calculation,
+ *  how much cubic woods needed for chair, table, and bed. 
+ */
+function woodCalculator(params) {
+    
+}
+
+/** brick-calculation, 
+ * how much brick will take in a building whether it's 1/2 or 1'st 10th or 11-20th or 20+th floors. 
+ * each feet in height will take 1000 bricks
+ * 1'st 10th floors will be 15' in height
+ * 11-20th floors will be 12' in height
+ * 20+th floors will be 10' in height
+ */
+function brickCalculator(params) {
+    
+}
+
+/** */
+function tinyFriend(params) {
+    
+}
+
